7bb6fa4b2d49ec32dcf3cee46956a59c45230043
unknown <q@q.q>
2023-12-19 23:30:08 +0300

    versionOne

---
+++ Veget/PageApp/PageStockWorker/PageMenuStock.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Veget.PageApp.PageStockWorker
+{
+    /// <summary>
+    /// Логика взаимодействия для PageMenuStock.xaml
+    /// </summary>
+    public partial class PageMenuStock : Page
+    {
+        public PageMenuStock()
+        {
+            InitializeComponent();
+        }
+    }
+}
